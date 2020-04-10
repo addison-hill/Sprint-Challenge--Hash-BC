bf0992096a57d73f68d5ebb97dbaa23f2c2e2579
Addison Hill <adddison.hill@gmail.com>
2020-04-10 10:41:38 -0500

    hashtables done, passing tests

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -16,8 +16,23 @@ def reconstruct_trip(tickets, length):
     hashtable = HashTable(length)
     route = [None] * length
 
-    """
-    YOUR CODE HERE
-    """
+    # insert takes in key, value we can insert (starting location, destination)
+    for ticket in tickets:
+        hash_table_insert(hashtable, ticket.source, ticket.destination)
 
-    pass
+    # Now we have all our tickets in hashtable, now we need to set up our route
+    # We can find first by retreiving the key with NONE
+    first_flight = hash_table_retrieve(hashtable, "NONE")
+
+    # We can find the next location by using retrieve of route[i-1]
+
+    # ith location can be found by checking hash for i - 1th location...might need to cleanup to pass tests
+    for i in range(length):
+        if i < 1:
+            route[i] = first_flight
+        else:
+            next_flight = hash_table_retrieve(hashtable, route[i-1])
+            route[i] = next_flight
+
+    route.remove("NONE")
+    return route
